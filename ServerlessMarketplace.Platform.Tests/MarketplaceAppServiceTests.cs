6abe60afe6d92937139db04c8b91529dcfc8a949
Pauloocm <paulooo.cm@gmail.com>
2024-04-12 20:06:46 -0300

    Added first test to build correcly CI/CD pipeline

---
+++ ServerlessMarketplace.Platform.Tests/MarketplaceAppServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using NSubstitute;
+using ServerlessMarketplace.Platform.Application;
+using ServerlessMarketplace.Platform.Application.Products;
+using ServerlessMarketplace.Platform.Infrastructure.Services;
+
+namespace ServerlessMarketplace.Platform.Tests
+{
+    [TestFixture]
+    public class MarketplaceAppServiceTests
+    {
+        private MarketplaceAppService appService;
+        private ISqsService SqsServ;
+
+        [SetUp]
+        public void Setup()
+        {
+            SqsServ = Substitute.For<ISqsService>();
+        }
+
+        [Test]
+        public void Add_Should_Throw_If_Command_Is_Invalid()
+        {
+            Assert.ThrowsAsync<NullReferenceException>(async () => await appService.Add((AddProductCommand)null, Arg.Any<CancellationToken>()));
+        }
+
+    }
+}
