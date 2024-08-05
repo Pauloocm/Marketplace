c2a2e95ae0bb20f42f1f87e1ea0fa51f250bc03d
pauloocm <paulooo.cm@gmail.com>
2024-08-04 21:17:16 -0300

    Implemented sqs message publisher

---
+++ ServerlessMarketplace.Platform/Infrastructure/Queue/ISqsPublisher.cs
@@ -0,0 +1,9 @@
+﻿using ServerlessMarketplace.Platform.Message;
+
+namespace ServerlessMarketplace.Platform.Infrastructure.Queue
+{
+    public interface ISqsPublisher
+    {
+        Task PublishMessage(ProductCreated productCreated, CancellationToken cancellationToken = default);
+    }
+}
---
+++ ServerlessMarketplace.Platform/Message/ProductCreated.cs
@@ -0,0 +1,6 @@
+﻿namespace ServerlessMarketplace.Platform.Message
+{
+    public record ProductCreated(Guid Id, string Name, string Description, decimal Price, string? Category)
+    {
+    }
+}
