d792d6d4bbb88a735bd52b8489d1d53b75d3af1f
pauloocm <paulooo.cm@gmail.com>
2025-02-16 21:21:18 -0300

    Implemented classes

--- ServerlessMarketplace.Domain/Categorys/Category.cs
+++ ServerlessMarketplace.Domain/Categorys/Category.cs
@@ -7,17 +7,14 @@ namespace ServerlessMarketplace.Domain.Categorys
         private static readonly List<Category> Categories = [];
 
         public static readonly Category Hardware = new(1, "Hardware");
-        public static readonly Category Eletrodomestico = new(2, "Eletrodomestico");
-        public static readonly Category Periferico = new(3, "Periferico");
+        public static readonly Category HouseholdAppliance = new(2, "Household Appliance");
+        public static readonly Category Peripheral = new(3, "Peripheral");
         public static readonly Category Tv = new(4, "Tv");
         public static readonly Category Smartphone = new(5, "Smartphone");
 
-        [Required]
-        public int Id { get; set; }
+        [Required] public int Id { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        public string Name { get; set; }
+        [Required] [StringLength(50)] public string Name { get; set; }
 
         public Category(int id, string name)
         {
@@ -35,6 +32,5 @@ namespace ServerlessMarketplace.Domain.Categorys
         {
             return Categories.SingleOrDefault(c => c.Id == id);
         }
-
     }
 }
