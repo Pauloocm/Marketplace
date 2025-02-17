3d038fe3e20878437c97d3b5e172fadfa8b681c3
pauloocm <paulooo.cm@gmail.com>
2025-02-17 07:47:41 -0300

    Add some maps

--- ServerlessMarketplace.Domain/Customer/Customer.cs
+++ ServerlessMarketplace.Domain/Customer/Customer.cs
@@ -5,6 +5,7 @@ namespace ServerlessMarketplace.Domain.Customer;
 
 public class Customer
 {
+    public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = null!;
     public int Age { get; set; }
     public string Email { get; set; } = null!;
---
+++ ServerlessMarketplace.Platform/Infrastructure/Database/Maps/AddressMap.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ServerlessMarketplace.Domain.Addresses;
+
+namespace ServerlessMarketplace.Platform.Infrastructure.Database.Maps
+{
+    public class AddressMap : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).ValueGeneratedNever();
+
+            builder.HasOne(p => p.Customer)
+                .WithOne(a => a.Address)
+                .HasForeignKey<Address>(a => a.CustomerId);
+
+            builder.Property(p => p.Country).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.State).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.City).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Street).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Number).HasMaxLength(5).IsRequired();
+            builder.Property(p => p.ZipCode).HasMaxLength(10).IsRequired();
+            builder.Property(p => p.Complement).HasMaxLength(200);
+        }
+    }
+}
